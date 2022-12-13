be74b43437a60fd45f20467b4fca7fe466af2c0c
Ilya <barutkinila@gmail.com>
2022-12-13 17:54:09 +0000

    fix lab 4.1, do lab4.2, scaffold lab4.3

--- lab4.1.py
+++ lab4.1.py
@@ -1,6 +1,5 @@
-from math import sqrt, cos, gamma
+from math import sqrt, cos
 from scipy import integrate
-from functools import lru_cache
 
 def f(x):
     return cos(x)
@@ -15,7 +14,7 @@ def F(a, b):
     return integrate.quad(lambda x: f(x)*weight(x), a, b)[0]
 
 
-def coeffs(nodes, a, b):
+def interpol_coeffs(nodes, a, b):
     n = len(nodes)
     res = []
 
@@ -28,16 +27,25 @@ def coeffs(nodes, a, b):
                 p *= (x - nodes[i])/(nodes[k] - nodes[i])
             return p * weight(x)
         res.append(integrate.quad(A, a, b)[0])
+
+    # for fixed nodes 1/4 and 3/4
+    # A1 = lambda x: 1/6 * sqrt(x)*(4*x - 9) / (1/4 - 3/4)
+    # A2 = lambda x: 1/6 * sqrt(x)*(4*x - 3) / (3/4 - 1/4)
+    # return [A1(b) - A1(a), A2(b) - A2(a)]
+
+    print ('A1 ', res[0])
+    print ('A2 ', res[1])
     return res
 
 
 def mid_rectangles(f, a, b):
-    return (b-a)*f(((a+b)/2))
+    h = (b-a)/2
+    return h*(f(a + h/2) + f(a + h/2 + h))
  
 
 def interpol(table, a, b):
     nodes, Y = zip(*table)
-    A = coeffs(nodes, a, b)
+    A = interpol_coeffs(nodes, a, b)
     S = 0
     for k, _ in enumerate(nodes):
         S += Y[k]*A[k]
@@ -48,9 +56,13 @@ def gauss(f, a, b):
     S = lambda x: (a+b)/2 + (b-a)/2*x
     return (b-a)/2*(f(S(x1))+f(S(x2)))
 
-def nast(f, weight, a, b):
+def nast_coeffs(weight, a, b):
     def m (k):
-        return integrate.quad(lambda x:weight(x) * x**k,a,b)[0]
+        # for fixed weight 1/sqrt(x)
+        ff = lambda x: x**(k+0.5)/(k+0.5)
+        return ff(b) - ff(a)
+        # return integrate.quad(lambda x:weight(x) * x**k,a,b)[0]
+    
     a2=(m(2)*m(2)-m(3)*m(1))/(m(1)*m(1)-m(2)*m(0))
     a1=(-m(2)-a2*m(0))/m(1)
 
@@ -59,16 +71,22 @@ def nast(f, weight, a, b):
     x2=(-a1-sqrt(D))/2
 
     A1=(m(1)-x2*m(0))/(x1-x2)
-    A2=(m(1)-x1*m(0))/(x2-x1)
+    A2=(m(1)-x1*m(0))/(x2-x1)  
+    return (A1, x1), (A2, x2)
+
+def nast(f, weight, a, b):
+    (A1, x1), (A2, x2) = nast_coeffs(weight, a, b)
 
     return f(x1)*A1+f(x2)*A2
 
 print ('Приближенное вычисление интегралов')
-print('Вариант 2 ∫cos(x)/√x dx')
+print('Вариант 2 J = ∫cos(x)/√x dx')
 
-a = 0 # float(input ('a = ')) # 0
-b = 1 # float(input ('b = ')) # 1
-nodes = [0.25, 0.75] # map(float, input('узлы через пробел').split(' ')) # [0.25, 0.75]
+print("Ввод пределов интегрирования")
+a =  float(input ('a = ')) # 0
+b =  float(input ('b = ')) # 1
+print('(только для интерполяционной формулы)')
+nodes = map(float, input('узлы через пробел: ').split(' ')) # [0.25, 0.75]
 table = list(map(lambda node: (node, f(node)), nodes))
 
 for (x,y) in table:
@@ -78,7 +96,8 @@ J1 = mid_rectangles(func, a, b)
 J2 = interpol(table, a, b)
 J3 = gauss(func, a, b)
 J4 = nast(f, weight, a, b)
-print('Формула средних прямоугольников %f, r=%f' % (J1, abs(J1-J)))
-print('Интерполяционная формула %f, r=%f' % (J2, abs(J2-J)))
-print('Формула Гаусса %f, r=%f' % (J3, abs(J3-J)))
-print('Формула Н.А.C.Т. %f, r=%f' % (J4, abs(J4-J)))
+print('"Точно"  J=%f' % J)
+print('Формула средних прямоугольников J=%f, фактическая погрешность %f' % (J1, abs(J1-J)))
+print('Интерполяционная формула J=%f, фактическая погрешность %f' % (J2, abs(J2-J)))
+print('Формула Гаусса J=%f, фактическая погрешность %f' % (J3, abs(J3-J)))
+print('Формула Н.А.C.Т. J=%f, фактическая погрешность %f' % (J4, abs(J4-J)))
---
+++ lab4.2.py
@@ -0,0 +1,54 @@
+from scipy import integrate
+from math import cos, sin, log, sqrt
+
+print ("Приближённое вычисление интеграла по квадратурным формулам")
+print ("Вариант 2 J = ∫cos(x)*x^2 dx")
+
+input_f = input ("Введите другую функцию для интегрирования или нажмите enter: ")
+if input_f.strip():
+    f = eval("lambda x: " + input_f.strip())
+else:
+    f = lambda x: cos(x)*x*x
+
+print("Ввод пределов интегрирования")
+a =  float(input ('a = ')) # 0
+b =  float(input ('b = ')) # 1
+
+J, _ = integrate.quad(f, a, b)
+
+print('Точное значение J=%f' % J)
+
+def left_rect(a, b):
+    return (a,), (1,)
+left_rect.title = 'КФ левого прямоугольника'
+
+def right_rect(a, b):
+    return  (b,), (1,)
+right_rect.title = 'КФ правого прямоугольника'
+
+def middle_rect(a, b):
+    return ((a+b)/2,), (1,)
+middle_rect.title = 'КФ среднего прямоугольника'
+
+def trapezoid(a, b):
+    return (a, b), (0.5, 0.5)
+trapezoid.title = 'КФ трапеции'
+
+def simpson(a, b):
+    return  (a, (a+b)/2, b), (1/6, 4/6, 1/6)
+simpson.title = 'КФ Симпсона'
+
+def three_over_eight(a, b):
+    return (a,(2*a+b)/3, (a+2*b)/3, b), (1/8, 3/8, 3/8, 1/8)
+three_over_eight.title = 'КФ 3/8'
+
+def calculate(f, method, a, b):
+    nodes, weights = method(a, b)
+    S = 0
+    for node, weight in zip(nodes, weights):
+        S += (b-a)*f(node)*weight 
+    return S
+
+for method in [left_rect, right_rect, middle_rect, trapezoid, simpson, three_over_eight]:
+    J1 =  calculate(f, method, a, b)
+    print('%s: J = %f, погрешность: %f' % (method.title, J1, abs(J-J1)))
